ada06e4977c4f4af5a240cc6ca4342c03690054d
pgudi <prabhakargudi@gmail.com>
2024-11-05 08:40:47 +0530

    Product Application

--- ProductApp/src/App.jsx
+++ ProductApp/src/App.jsx
@@ -1,10 +1,23 @@
 import DisplayProducts from "./components/DisplayProducts"
-
+import FooterComponent from "./components/FooterComponent"
+import HeaderComponent from "./components/HeaderComponent"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import ProductComponent from "./components/ProductComponent"
 function App() {
-  
+
   return (
     <>
-       <DisplayProducts />
+      <BrowserRouter>
+          <HeaderComponent />
+          <Routes>
+            <Route path="/" element={<DisplayProducts />}></Route>
+            <Route path="/products" element={<DisplayProducts />}></Route>
+            <Route path="/createproduct" element={<ProductComponent />} ></Route>
+            <Route path="/editproduct/:id" element={<ProductComponent />} ></Route>
+          </Routes>
+          <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
